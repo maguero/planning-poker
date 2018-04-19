cc85de6fe77d26e9481c6f9d3dbcb7aadfa20e8f
jhelguero <jhelguero@grubhub.com>
2018-04-19 00:07:24 -0300

    006 - New header page component

---
+++ src/app/header-page/header-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'header-page',
+  templateUrl: './header-page.component.html',
+  styleUrls: ['./header-page.component.css']
+})
+export class HeaderPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
