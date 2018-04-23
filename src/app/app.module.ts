ec79ced690c171a5c7484dd39d68329143ae6d14
Matias Aguero <mjaguero.work@gmail.com>
2018-04-23 11:56:11 -0300

    #11 : My Cards componnent with only card images.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PlanningRoomComponent } from './planning-room/planning-room.component';
 import { HeaderPageComponent } from './header-page/header-page.component';
+import { MyCardsComponent } from './my-cards/my-cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PlanningRoomComponent,
-    HeaderPageComponent
+    HeaderPageComponent,
+    MyCardsComponent
   ],
   imports: [
     BrowserModule
