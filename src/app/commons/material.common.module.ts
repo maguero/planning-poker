9ac835169a75f0b50a27a898b7da006c531da940
Matias Aguero <mjaguero.work@gmail.com>
2018-11-02 14:41:37 -0300

    #44 HomePage updated with new design and to use Material.

--- src/app/commons/material.common.module.ts
+++ src/app/commons/material.common.module.ts
@@ -1,13 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatFormFieldModule, MatInputModule } from '@angular/material';
+import { MatDividerModule } from '@angular/material/divider';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     imports: [ 
-        CommonModule 
+        CommonModule
     ],
     exports: [
-        MatButtonModule
+        MatButtonModule,
+        MatGridListModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatDividerModule,
+        FormsModule,
+        ReactiveFormsModule
     ]
 })
 
